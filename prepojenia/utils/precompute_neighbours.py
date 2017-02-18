72377b7ae7efe5c2e9d861ee6b444257491c7cb9
User Matej_balog <matej_balog@www00.c.tulip-01.internal>
2017-02-18 12:08:38 +0100

    initial skeleton of prepojenia utils

---
+++ prepojenia/utils/precompute_neighbours.py
@@ -0,0 +1,4 @@
+import read_entities
+
+if __name__ == '__main__':
+    print read_entities.read_entities_mock()
---
+++ prepojenia/utils/read_entities.py
@@ -0,0 +1,2 @@
+def read_entities_mock():
+    return ["Jan Novak", "Jana Novakova", "Peter Novak"],[22, 41, 77],[0.15, 0.18, 0.18],[0.22, 0.19, 0.19] 
