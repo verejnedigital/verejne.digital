b2e42049a9fe2445e99bab75b4417ed6835f9e97
User Bundala <bundala@www00.c.tulip-01.internal>
2017-04-17 18:41:21 +0200

    Removing unnecessary params from config

--- data/db.py
+++ data/db.py
@@ -4,4 +4,4 @@ import yaml
 def db_connect():
     with open("db_config.yaml", "r") as stream:
         config = yaml.load(stream)
-    return psycopg2.connect(user=config["user"], dbname=config["db"])
+        return psycopg2.connect(user=config["user"], dbname=config["db"])
