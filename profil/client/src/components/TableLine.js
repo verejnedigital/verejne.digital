950ce3c6436ca57045f0909c88a1bc9021da85e3
Rasto <rasto@Rastislavs-MacBook-Air.local>
2017-11-25 17:44:38 +0100

    small tableline change

--- profil/client/src/components/TableLine.js
+++ profil/client/src/components/TableLine.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const TableLine = ({politician}) => (
 <tr>
+  <td><img alt="profile photo" class="thumb-photo" src="http://www.nrsr.sk/web/dynamic/PoslanecPhoto.aspx?PoslanecID=1&amp;ImageWidth=140"/></td>
   <td>{politician.firstname} {politician.surname}</td>
   <td className="party-column">{politician.party_nom}</td>
   <td className="number-column">8</td>
