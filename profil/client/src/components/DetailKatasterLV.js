805c81bd161605371c22004cb26355e8a6cb33ca
Milutin Kristofic <milutin.kristofic@gmail.com>
2017-11-25 20:31:33 +0100

    Update Detail Page

--- profil/client/src/components/DetailKatasterLV.js
+++ profil/client/src/components/DetailKatasterLV.js
@@ -3,14 +3,16 @@ import React from 'react';
 
 import './DetailKatasterLV.css';
 
-const DetailKatasterLV = ({lv}) => (
+const DetailKatasterLV = ({num, lv}) => (
 <tr>
+  <td>{num}
+  </td>
   <td>
     <div>
       <a href="">{lv.landusename}</a>
       <span className="podiel">{`${Number(lv.participantratio).toFixed(2)}% podiel`}</span>
     </div>
-    <div>{`${lv.cadastralunitname}, cislo parcely: ${lv.parcelno}; LV c. ${lv.foliono}`}</div>
+    <div>{`${lv.cadastralunitname}, číslo parcely: ${lv.parcelno}; LV č. ${lv.foliono}`}</div>
   </td>
 </tr>
 );
