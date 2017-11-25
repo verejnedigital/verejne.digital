8b1847281f96c35f1435455b26e2fb341d7d60f1
Milutin Kristofic <milutin.kristofic@gmail.com>
2017-11-25 16:47:34 +0100

    Cleaning columns

--- profil/client/src/components/TableLine.js
+++ profil/client/src/components/TableLine.js
@@ -3,10 +3,10 @@ import React from 'react';
 const TableLine = ({politician}) => (
 <tr>
   <td>{politician.firstname} {politician.surname}</td>
-  <td>{politician.party_nom}</td>
-  <td className="numbercolumn">8</td>
-  <td className="numbercolumn">24</td>
-  <td className="numbercolumn">3</td>
+  <td className="party-column">{politician.party_nom}</td>
+  <td className="number-column">8</td>
+  <td className="number-column">24</td>
+  <td className="number-column">3</td>
 </tr>
 );
 export default TableLine;
