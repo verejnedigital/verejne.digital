925367de0babf32284253ad68cbb5a102c659de2
Milutin Kristofic <milutin.kristofic@gmail.com>
2017-11-25 12:38:08 +0100

    Start profil react client

---
+++ profil/client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div className="App-title">
+            New New Social Network
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
