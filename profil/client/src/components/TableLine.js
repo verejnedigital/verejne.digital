a338bcb36f25cd6a673a361be6a43632b0c2a264
Matej Balog <matej.balog@gmail.com>
2017-11-27 01:56:05 +0100

    profil frontend property counts from kataster

--- profil/client/src/components/TableLine.js
+++ profil/client/src/components/TableLine.js
@@ -5,9 +5,9 @@ const TableLine = ({politician}) => (
   <td><img alt="profile" className="thumb-photo" src="http://www.nrsr.sk/web/dynamic/PoslanecPhoto.aspx?PoslanecID=1&amp;ImageWidth=140"/></td>  
   <td><a href={`detail/${politician.id}`}>{politician.firstname} {politician.surname}</a></td>
   <td className="party-column">{politician.party_abbreviation}</td>
-  <td className="number-column">8</td>
-  <td className="number-column">24</td>
-  <td className="number-column">3</td>
+  <td className="number-column">{politician.num_houses_flats}</td>
+  <td className="number-column">{politician.num_fields_gardens}</td>
+  <td className="number-column">{politician.num_others}</td>
 </tr>
 );
 export default TableLine;
