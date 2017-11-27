39b322e18267148d29c67983ad7266235e765efb
Matej Balog <matej.balog@gmail.com>
2017-11-27 12:40:41 +0100

    kataster ListPoliticians: do not double count parcels within one folio

--- kataster/db_search.py
+++ kataster/db_search.py
@@ -98,14 +98,27 @@ def get_Parcels_from_database(db, person, search_params):
 def count_Parcels_in_database(db):
     q = """
         WITH
-        Parcels AS (
+        ParcelsAll AS (
             SELECT FolioId, No, Area, Id, LandUseId, UtilisationId, CadastralUnitId, ValidTo, 'C' AS ParcelType FROM kataster.ParcelsC
             UNION
             SELECT FolioId, No, Area, Id, LandUseId, UtilisationId, CadastralUnitId, ValidTo, 'E' AS ParcelType FROM kataster.ParcelsE
         ),
+        Parcels AS (
+            SELECT DISTINCT ON (CadastralUnitCode, FolioNo, No)
+                ParcelsAll.*,
+                kataster.CadastralUnits.Code AS CadastralUnitCode,
+                kataster.Folios.No AS FolioNo
+            FROM
+                ParcelsAll
+            INNER JOIN
+                kataster.Folios ON kataster.Folios.Id = ParcelsAll.FolioId
+            INNER JOIN
+                kataster.CadastralUnits ON kataster.CadastralUnits.Id = ParcelsAll.CadastralUnitId
+            ORDER BY
+                CadastralUnitCode, FolioNo, No, ValidTo DESC
+        ),
         ParcelsOwned AS (
-            SELECT DISTINCT ON (CadastralUnitCode, FolioNo, ParcelNo)
-                Parcels.No AS ParcelNo,
+            SELECT DISTINCT
                 kataster.Folios.No AS FolioNo,
                 kataster.LandUses.Name AS LandUseName,
                 kataster.CadastralUnits.Code AS CadastralUnitCode,
@@ -130,8 +143,6 @@ def count_Parcels_in_database(db):
                 kataster.CadastralUnits ON kataster.CadastralUnits.Id = Parcels.CadastralUnitId
             WHERE
                 kataster.Participants.TypeId=1
-            ORDER BY
-                CadastralUnitCode, FolioNo, ParcelNo, Parcels.ValidTo DESC
         )
         SELECT
             FirstNameSearch, SurnameSearch, DobHash, LandUseName, COUNT(*) AS GroupCount
