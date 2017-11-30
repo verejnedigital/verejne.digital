4ea0f5590cf3d4990f66766b1d697101818c4e99
Rasto <rasto@Rastislavs-MacBook-Air.local>
2017-11-30 15:14:51 +0100

    fix_link 2

--- profil/client/src/App.js
+++ profil/client/src/App.js
@@ -45,8 +45,8 @@ class App extends Component {
   }
 
   render() {
-    return (this.props.params.splat.length > 0 && this.props.params.splat !== "profil") ?
-     (<DetailPage id={this.props.params.splat} />) :
+    return (this.props.params.splat.length > 0 && this.props.params.splat !== "profil" && this.props.params.splat !== "profil/") ?
+     (<DetailPage id={this.props.params.splat.replace("profil/", "")} />) :
      (
       <div className="App">        
         <div className="App-header">
