76c0bd631a544c2534255bf3f07560454e75278c
Matej Balog <matej.balog@gmail.com>
2017-12-14 00:37:11 +0100

    kataster ListPoliticians: single SQL query computation

--- kataster/db_search.py
+++ kataster/db_search.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from db import db_query
 from utils import search_string, hash_timestamp, Mercator_to_WGS84
 
@@ -109,12 +111,12 @@ def get_Parcels_from_database(db, person, search_params):
 
     return rows
 
-def count_Parcels_in_database(db):
+def get_politicians_with_Folio_counts(db):
     q = """
         WITH
         ParcelsAll AS (
             SELECT FolioId, No, Area, Id, LandUseId, UtilisationId, CadastralUnitId, ValidTo, 'C' AS ParcelType FROM kataster.ParcelsC
-            UNION
+            UNION ALL
             SELECT FolioId, No, Area, Id, LandUseId, UtilisationId, CadastralUnitId, ValidTo, 'E' AS ParcelType FROM kataster.ParcelsE
         ),
         Parcels AS (
@@ -136,9 +138,9 @@ def count_Parcels_in_database(db):
                 kataster.Folios.No AS FolioNo,
                 kataster.LandUses.Name AS LandUseName,
                 kataster.CadastralUnits.Code AS CadastralUnitCode,
-                kataster.Subjects.FirstNameSearch AS FirstNameSearch,
-                kataster.Subjects.SurnameSearch AS SurnameSearch,
-                kataster.Subjects.DobHash AS DobHash
+                kataster.Subjects.FirstNameSearch AS SubjectFirstNameSearch,
+                kataster.Subjects.SurnameSearch AS SubjectSurnameSearch,
+                kataster.Subjects.DobHash AS SubjectDobHash
             FROM
                 Parcels
             INNER JOIN
@@ -157,13 +159,47 @@ def count_Parcels_in_database(db):
                 kataster.CadastralUnits ON kataster.CadastralUnits.Id = Parcels.CadastralUnitId
             WHERE
                 kataster.Participants.TypeId=1
+        ),
+        PoliticianCounts AS (
+            SELECT
+                kataster.politicians.Id AS Id,
+                kataster.politicians.FirstName AS FirstName,
+                kataster.politicians.Surname AS Surname,
+                kataster.politicians.Title AS Title,
+                COUNT(*) FILTER (WHERE LandUseName = 'Zastavaná plocha a nádvorie') AS num_houses_flats,
+                COUNT(*) FILTER (WHERE LandUseName = 'Orná pôda' OR LandUseName = 'Záhrada') AS num_fields_gardens,
+                COUNT(*) FILTER (WHERE LandUseName != 'Zastavaná plocha a nádvorie' AND LandUseName != 'Orná pôda' AND LandUseName != 'Záhrada') AS num_others
+            FROM
+                kataster.politicians
+            LEFT OUTER JOIN
+                ParcelsOwned ON
+                    ParcelsOwned.SubjectFirstNameSearch = kataster.politicians.FirstNameSearch AND
+                    ParcelsOwned.SubjectSurnameSearch = kataster.politicians.SurnameSearch AND
+                    ParcelsOwned.SubjectDobHash = kataster.politicians.DobHash
+            GROUP BY
+                Id, FirstName, Surname, Title
         )
-        SELECT
-            FirstNameSearch, SurnameSearch, DobHash, LandUseName, COUNT(*) AS GroupCount
+        SELECT DISTINCT ON (PoliticianCounts.Id)
+            PoliticianCounts.*,
+            kataster.politicianterms.picture_url AS picture,
+            kataster.parties.abbreviation AS party_abbreviation,
+            kataster.parties.name AS party_nom,
+            kataster.terms.start AS term_start,
+            kataster.terms.finish AS term_finish,
+            kataster.offices.name_male AS office_name_male,
+            kataster.offices.name_female AS office_name_female
         FROM
-            ParcelsOwned
-        GROUP BY
-            FirstNameSearch, SurnameSearch, DobHash, LandUseName
+            PoliticianCounts
+        JOIN
+            kataster.politicianterms ON kataster.politicianterms.politicianid=Id
+        JOIN
+            kataster.terms ON kataster.terms.id=kataster.politicianterms.termid
+        JOIN
+            kataster.offices ON kataster.offices.id=kataster.terms.officeid
+        JOIN
+            kataster.parties ON kataster.parties.id=kataster.politicianterms.party_nomid
+        ORDER BY
+            PoliticianCounts.Id, kataster.terms.finish DESC
         ;"""
     rows = db_query(db, q)
     return rows
