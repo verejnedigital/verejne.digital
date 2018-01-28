c2e627cfbace61d2fa03b0d6ff7dae3c392cbc6d
Matej Balog <matej.balog@gmail.com>
2018-01-28 19:35:47 +0100

    prepojenia precompute_neighbours consolidate_people: more work to be performed on the database side

---
+++ data/db/db.py
@@ -0,0 +1,41 @@
+import psycopg2
+import psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+import yaml
+
+class DatabaseConnection():
+    def __init__(self, path_config='db_config.yaml', search_path=None):
+        with open(path_config, 'r') as stream:
+            config = yaml.load(stream)
+        self.conn = psycopg2.connect(user=config['user'], dbname=config['db'])
+        if search_path is not None:
+            self.execute('SET search_path = %s', (search_path,))
+
+    def execute(self, query, query_data=()):
+        """ Executes query without returning any data """
+        with self.conn.cursor() as cur:
+            cur.execute(query, query_data)
+
+    def query(self, query, query_data=()):
+        """ Executes query and returns all rows as a list of dicts """
+        with self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+            cur.execute(query, query_data)
+            rows = cur.fetchall()
+        return rows
+
+    def get_server_side_cursor(self, query, query_data=(), buffer_size=100000):
+        """ Executes query and returns all rows using a server side cursor """
+        cur = self.conn.cursor('dummy_cursor_name')
+        cur.itersize = buffer_size
+        cur.execute(query, query_data)
+        return cur
+
+    def dict_cursor(self):
+        return self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    def commit(self):
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
