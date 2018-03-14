8c79843a7dbd02db8c7b57f24ef65eaa5213a90a
Rasto <rastislav.lenhardt@gmail.com>
2018-03-14 16:28:58 +0100

    print progress

--- data/generate_prod_data.py
+++ data/generate_prod_data.py
@@ -124,7 +124,9 @@ def ProcessSource(db_prod, geocoder, entities, config):
 
             eid = entities.GetEntity(row["ico"], name, addressId)
             # print name, "-> eid:", eid
-            
+            if found%20000==0:
+                print "Progress:",found
+
             if config.get("save_org_id"):
                 entities.AddOrg2Eid(row["org_id"], eid)
             if config.get("use_org_id_as_eid_relation"):
