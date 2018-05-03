9506d6dbeb146d50af48b6b2abd4104b98d193eb
Martin Pinter <martin.pinter@vacuumlabs.com>
2018-05-03 12:23:17 +0200

    Flow example, reducer fixes, remove object-path-immutable

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,13 +4,15 @@ import Landing from './Landing'
 import Prepojenia from './Prepojenia'
 import Verejne from './Verejne'
 import Obstaravania from './Obstaravania'
+import Example from './Example'
 
 const App = () => (
   <Switch>
     <Route path="/" exact component={Landing} />
-    <Route path="/verejne" exact component={Verejne} />
+    <Route path="/verejne" component={Verejne} />
     <Route path="/prepojenia" component={Prepojenia} />
-    <Route path="/obstaravania" exact component={Obstaravania} />
+    <Route path="/obstaravania" component={Obstaravania} />
+    <Route path="/example" component={Example} />
     <Redirect to="/" />
   </Switch>
 )
