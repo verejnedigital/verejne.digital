3dd29275e6a2f701f1ebbc19f9cfb40fb7fd8fa2
Siegrift <e.tesarr@gmail.com>
2018-05-10 13:31:53 +0200

    Fix legend

---
+++ client/src/components/Verejne/Legend/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import CircleIcon from 'react-icons/lib/fa/circle-o'
+import FilledCircleIcon from 'react-icons/lib/fa/circle'
+import classnames from 'classnames'
+import {withState} from 'recompose'
+import {FACEBOOK_LIKE_SRC} from '../../../constants'
+import './Legend.css'
+
+const Legend = ({legendOpen, setLegendOpen}) => (
+  <div className={classnames('Legend', {Hidden: !legendOpen})} >
+    <div className="Legend__Header">
+      <iframe src={FACEBOOK_LIKE_SRC} width="145px" height="20px" scrolling="no" frameBorder="0" />
+      <button type="button" className="close" onClick={() => setLegendOpen(false)}><span>&times;</span></button>
+    </div>
+    <p>
+      <CircleIcon className="Svg" />
+        Firma / Osoba
+    </p>
+    <p>
+      <FilledCircleIcon className="Svg" />
+        Obchod so štátom
+    </p>
+    <p>
+      <CircleIcon className="Svg Orange" />
+      Kontakt s politikou
+    </p >
+    <p>
+      <FilledCircleIcon className="Svg Orange" />
+      Kontakt s politikou a obchod so štátom
+    </p >
+  </div >
+)
+
+export default withState('legendOpen', 'setLegendOpen', true)(Legend)
