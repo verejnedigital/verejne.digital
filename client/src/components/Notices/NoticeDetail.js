6dd92357646fcba844ae15130cb0f4686e093d5f
Milan Darjanin <milan.darjanin@gmail.com>
2018-07-13 15:58:33 +0200

    Refactor renders for NoticeDetail

--- client/src/components/Notices/NoticeDetail.js
+++ client/src/components/Notices/NoticeDetail.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import React, {Fragment} from 'react'
 import {compose} from 'redux'
 import {connect} from 'react-redux'
 import {withDataProviders} from 'data-provider'
@@ -10,7 +10,7 @@ import {getSuspectLevelLimit, ShowNumberCurrency} from './utilities'
 import CompaniesTable from './CompaniesTable'
 import './NoticeDetail.css'
 import NoticeInformation from './NoticeInformation'
-import {Container, Row, Col} from 'reactstrap'
+import {Container} from 'reactstrap'
 
 import type {Notice, State} from '../../state'
 
@@ -33,59 +33,49 @@ const NoticeDetail = ({notice}: NoticeDetailProps) => {
       >
         {`${notice.bulletin_number}/${notice.bulletin_year}`}
       </ExternalLink>
-      <span className="note">({notice.bulletin_date})</span>
+      <strong>({notice.bulletin_date})</strong>
     </span>
   ) : null
 
   const estimate =
-    notice.price_num >= 5
-      ? [
-        <ShowNumberCurrency num={getSuspectLevelLimit(notice, -1)} key="currency-1" />,
-        ' - ',
-        <ShowNumberCurrency num={getSuspectLevelLimit(notice, 1)} key="currency-2" />,
-      ]
-      : null
+    notice.price_num >= 5 ? (
+      <Fragment>
+        <ShowNumberCurrency num={getSuspectLevelLimit(notice, -1)} key="currency-1" />
+        {' - '}
+        <ShowNumberCurrency num={getSuspectLevelLimit(notice, 1)} key="currency-2" />
+      </Fragment>
+    ) : null
 
   const noticeDetailInformations = [
     {
-      label: <span className="my-label">Popis:</span>,
+      label: 'Popis',
       body: notice.text,
     },
     {
-      label: <span className="my-label">Objednávateľ:</span>,
+      label: 'Objednávateľ',
       body: notice.customer,
     },
     {
-      label: <span className="my-label">Vestník:</span>,
+      label: 'Vestník',
       body: bulletin,
     },
     {
-      label: <span className="my-label">Vyhlásená cena:</span>,
+      label: 'Vyhlásená cena',
       body: <ShowNumberCurrency num={notice.price} />,
     },
     {
-      label: <span className="my-label">Náš odhad:</span>,
+      label: 'Náš odhad',
       body: estimate,
     },
-  ]
+  ].filter((item) => item.body !== null)
 
   return (
-    <Container tag="article" className="container-fluid notice-detail">
-      <Row>
-        <Col className="col-sm-12 col-xs-12">
-          <h3 className="title">{notice.title}</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col tag="section" className="col-sm-12 col-xs-12">
-          <NoticeInformation data={noticeDetailInformations.filter((o) => o.body !== null)} />
-        </Col>
-      </Row>
-      <Row>
-        <Col tag="section" className="col-sm-12 col-xs-12">
-          <CompaniesTable item={notice} />
-        </Col>
-      </Row>
+    <Container tag="article" className="notice-detail">
+      <h1 className="notice-detail-title">{notice.title}</h1>
+      <NoticeInformation data={noticeDetailInformations} />
+      <div className="notice-detail-table">
+        <CompaniesTable item={notice} />
+      </div>
     </Container>
   )
 }
