40712e834271d6d07008d62da7227b66696f82d5
Erik Kučák <riko@ksp.sk>
2018-08-03 13:08:07 +0200

    Changing URL at work with the map

--- client/src/components/Verejne/Map/index.js
+++ client/src/components/Verejne/Map/index.js
@@ -12,15 +12,20 @@ import {setMapOptions} from '../../../actions/verejneActions'
 import './GoogleMap.css'
 import {map} from 'lodash'
 import ClusterMarker from './ClusterMarker'
-import {branch, compose, renderComponent, withHandlers} from 'recompose'
+import {branch, compose, renderComponent, withHandlers, lifecycle} from 'recompose'
 import Loading from '../../Loading'
 import GoogleMap from '../../GoogleMap'
 import {withDataProviders} from 'data-provider'
 import {addressesProvider} from '../../../dataProviders/publiclyDataProviders'
+import {withRouter} from 'react-router-dom'
+import qs from 'qs'
+import {DEFAULT_MAP_CENTER, COUNTRY_ZOOM} from '../../../constants'
+import debounce from 'lodash/debounce'
 
 import type {MapOptions, Entity} from '../../../state'
 import type {MapCluster} from '../../../selectors'
 import type {GenericAction} from '../../../types/reduxTypes'
+import type {RouterHistory} from 'react-router'
 
 type Props = {
   zoom: number,
@@ -29,6 +34,7 @@ type Props = {
   setMapOptions: (mapOptions: MapOptions) => GenericAction<MapOptions, MapOptions>,
   clusters: Array<MapCluster>,
   onChange: (options: MapOptions) => any,
+  history: RouterHistory,
 }
 
 // NOTE: there can be multiple points on the map on the same location...
@@ -40,7 +46,7 @@ const Map = ({
 }: Props) => {
   return (
     <div className="google-map-wrapper">
-      <GoogleMap center={center} zoom={zoom} onChange={onChange}>
+      <GoogleMap center={center} zoom={zoom} onChange={debounce(onChange, 2000)}>
         {map(clusters, (cluster, i) => (
           <ClusterMarker
             key={i}
@@ -70,10 +76,25 @@ export default compose(
   ),
   withDataProviders(({addressesUrl}) =>
     [addressesProvider(addressesUrl)]),
+  withRouter,
   withHandlers({
-    onChange: ({setMapOptions}) => (options) => {
-      setMapOptions(options)
-    },
+      onChange: (props) => (options) =>  {
+        props.setMapOptions(options)
+        props.history.replace(
+          `?lat=${options.center.lat}&lng=${options.center.lng}&zoom=${options.zoom}`
+        )
+      }
+  }),
+  lifecycle({
+    componentDidMount() {
+      const params = qs.parse(this.props.history.location.search.substr(1))
+      params.lat = params.lat === undefined ? DEFAULT_MAP_CENTER.lat : params.lat
+      params.lng = params.lng === undefined ? DEFAULT_MAP_CENTER.lng : params.lng
+      params.zoom = params.zoom === undefined ? COUNTRY_ZOOM : params.zoom
+      this.props.setMapOptions(
+        {center: [Number(params.lat), Number(params.lng)], zoom: Number(params.zoom)}
+      )
+    }
   }),
   // display loading only before first fetch
   branch((props) => !props.addresses, renderComponent(Loading))
