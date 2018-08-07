f1add7bc4ab5c34d3692aca3850fb0681fda4356
mirecmrozek <miroslav.mrozek@vacuumlabs.com>
2018-08-07 09:00:23 +0200

    add company details in verejne sidebar

--- client/src/components/Verejne/Map/AddressDetail/ListRow.js
+++ client/src/components/Verejne/Map/AddressDetail/ListRow.js
@@ -4,17 +4,49 @@ import {compose} from 'recompose'
 import {withDataProviders} from 'data-provider'
 import CircleIcon from 'react-icons/lib/fa/circle-o'
 import {ListGroupItem} from 'reactstrap'
+import { icoUrl, showDate, } from '../../../Notices/utilities'
+import ExternalLink from '../../../shared/ExternalLink'
 import {toggleEntityInfo} from '../../../../actions/verejneActions'
 import {entityDetailProvider} from '../../../../dataProviders/publiclyDataProviders'
 import {entityDetailSelector} from '../../../../selectors'
 import './ListRow.css'
-//import Info from './Info'
+
+const Item = ({children, label, url, linkText}) => (
+  <li className="info-item">
+    {label && <strong className="info-item-label">{label}</strong>}
+    {url && (
+      <ExternalLink isMapView={false} url={url}>
+        {linkText}
+      </ExternalLink>
+    )}
+    {children}
+  </li>
+)
 
 const _DetailedInfo = ({id, toggleEntityInfo, data}) => (
-  <ListGroupItem className="list-row" onClick={() => toggleEntityInfo(id)}>
-    There will be detail soon
-    {console.log(data, 'a')}
-    {/*<Info data={{entities: [data]}} canClose onClose={() => toggleEntityInfo(id)} />*/}
+  <ListGroupItem className="list-row">
+    {console.log(data)}
+    <div action className={data.companyinfo ? "info-header list-group-item-action" : "list-group-item-action"} onClick={() => toggleEntityInfo(id)}>
+      {!data.companyinfo && <CircleIcon size="15" className="list-row-icon" />}
+      {data.name}
+    </div>
+    {data.companyinfo &&
+    <div className="info-main">
+      <ul className="info-list">
+        <Item
+          label="IČO"
+          url={`http://www.orsr.sk/hladaj_ico.asp?ICO=${data.companyinfo.ico}&SID=0`}
+          linkText={data.companyinfo.ico}
+        >
+          (<ExternalLink isMapView={false} url={icoUrl(data.companyinfo.ico)}>
+            Detaily o firme
+          </ExternalLink>)
+        </Item>
+        {data.companyinfo.established_on && <Item label="Založená">{showDate(data.companyinfo.established_on)}</Item>}
+        {data.companyinfo.terminated_on && <Item label="Zaniknutá">{showDate(data.companyinfo.terminated_on)}</Item>}
+      </ul>
+    </div>
+    }
   </ListGroupItem>
 )
 
