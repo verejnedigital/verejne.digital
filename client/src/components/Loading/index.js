d5a9b249aaa7cf1c2e7a747ae3c6b2211c756d3d
Tomas Rybar <tomas.rybar@vacuumlabs.com>
2018-08-14 16:03:56 +0200

    added the spinning circle color as constant

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -4,6 +4,7 @@ import LoadingComponent from 'react-loading-components'
 import './Loading.css'
 import {defaultProps} from 'recompose'
 import {ListGroupItem} from 'reactstrap'
+import {LOADING_CIRCLE_COLOR} from '../../constants'
 
 type Props = {
   width: number,
@@ -12,19 +13,20 @@ type Props = {
 
 const Loading = ({width, height}: Props) => (
   <div className="loading">
-    <LoadingComponent type="tail_spin" width={width} height={height} fill="#0062db" />
+    <LoadingComponent type="tail_spin" width={width} height={height} fill={LOADING_CIRCLE_COLOR} />
   </div>
 )
 export const EntityDetailLoading = () => (
   <ListGroupItem className="list-row">
     <div className="loadingsmall list-row-item">
-      <LoadingComponent type="tail_spin" width={15} height={15} fill="#0062db" />
+      <LoadingComponent type="tail_spin" width={15} height={15} fill={LOADING_CIRCLE_COLOR} />
     </div>
   </ListGroupItem>
 )
 export const ModalLoading = () => (
   <div className="loading-modal">
-    <LoadingComponent type="tail_spin" width={250} height={250} fill="#0062db" />
+    <LoadingComponent type="tail_spin" width={250} height={250} fill={LOADING_CIRCLE_COLOR} />
+    {console.log(LOADING_CIRCLE_COLOR)}
   </div>
 )
 export default defaultProps({
