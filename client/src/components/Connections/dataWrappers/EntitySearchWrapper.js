1e698f9401259b4cd313c1ceb583ef6f97456e11
Jakub Ukrop <jakub.ukrop@vacuumlabs.com>
2018-08-15 14:11:48 +0200

    Fix showGraph parsing and keepAlive, remove dead code, minor refactor

--- client/src/components/Connections/dataWrappers/EntitySearchWrapper.js
+++ client/src/components/Connections/dataWrappers/EntitySearchWrapper.js
@@ -18,7 +18,7 @@ const EntitySearchWrapper = (WrappedComponent: ComponentType<*>) => {
         {...props}
         entitySearch1={query.eid1 || ''}
         entitySearch2={query.eid2 || ''}
-        showGraph={query.graph || false}
+        showGraph={query.graph === 'true' || false}
       />
     )
   }
