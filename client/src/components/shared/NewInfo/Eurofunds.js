5a1135325da066318808f22c90a2e3cb19fb3413
Jakub Ukrop <jakub.ukrop@vacuumlabs.com>
2018-08-17 16:24:05 +0200

    Refactor iteration keys ex extra returns in Info

--- client/src/components/shared/NewInfo/Eurofunds.js
+++ client/src/components/shared/NewInfo/Eurofunds.js
@@ -8,29 +8,28 @@ import ExternalLink from '../ExternalLink'
 import {ShowNumberCurrency} from '../../../services/utilities'
 import '../Info/InfoButton.css'
 
-const EuroFunds = ({data, toggledOn, toggle}) => {
-  return (
-    <div className="eurofunds info-button">
-      <Button outline color="primary" onClick={toggle}>
-        {toggledOn ? <ChevronUp aria-hidden="true" /> : <ChevronDown aria-hidden="true" />}
-        &nbsp;Eurofondy:&nbsp;<ShowNumberCurrency num={data.eufunds_price_sum} />&nbsp;
-        <Badge color="primary">{data.eufunds_count}</Badge>
-      </Button>
-      {toggledOn && (
-        <ul className="list-unstyled info-button-list">
-          {data.largest.map((eufund, i) => (
-            <li key={i}>
-              <ExternalLink url={eufund.link}>
-                {`${eufund.title}, `}
-                <ShowNumberCurrency num={eufund.price} />
-              </ExternalLink>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  )
-}
+const EuroFunds = ({data, toggledOn, toggle}) => (
+  <div className="eurofunds info-button">
+    <Button outline color="primary" onClick={toggle}>
+      {toggledOn ? <ChevronUp aria-hidden="true" /> : <ChevronDown aria-hidden="true" />}
+      &nbsp;Eurofondy:&nbsp;<ShowNumberCurrency num={data.eufunds_price_sum} />&nbsp;
+      <Badge color="primary">{data.eufunds_count}</Badge>
+    </Button>
+    {toggledOn && (
+      <ul className="list-unstyled info-button-list">
+        {data.largest.map((eufund, i) => (
+          // no proper ID available
+          <li key={i}>
+            <ExternalLink url={eufund.link}>
+              {`${eufund.title}, `}
+              <ShowNumberCurrency num={eufund.price} />
+            </ExternalLink>
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+)
 
 export default compose(
   withState('toggledOn', 'toggle', false),
--- client/src/components/shared/NewInfo/Notices.js
+++ client/src/components/shared/NewInfo/Notices.js
@@ -7,29 +7,27 @@ import {Badge, Button} from 'reactstrap'
 import {ShowNumberCurrency} from '../../../services/utilities'
 import '../Info/InfoButton.css'
 
-const Notices = ({data, toggledOn, toggle}) => {
-  return (
-    <div className="notices info-button">
-      <Button outline color="primary" onClick={toggle}>
-        {toggledOn ? <ChevronUp aria-hidden="true" /> : <ChevronDown aria-hidden="true" />}
-        &nbsp;Zmluvy so štátom:&nbsp;<ShowNumberCurrency
-          num={data.total_final_value_amount_eur_sum}
-        />&nbsp;
-        <Badge color="primary">{data.count}</Badge>
-      </Button>
-      {toggledOn && (
-        <ul className="list-unstyled info-button-list">
-          {data.most_recent.map((notice, i) => (
-            <li key={i} title={notice.title}>
-              {`${notice.client_name}, `}
-              <ShowNumberCurrency num={notice.total_final_value_amount} />
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  )
-}
+const Notices = ({data, toggledOn, toggle}) => (
+  <div className="notices info-button">
+    <Button outline color="primary" onClick={toggle}>
+      {toggledOn ? <ChevronUp aria-hidden="true" /> : <ChevronDown aria-hidden="true" />}
+      &nbsp;Zmluvy so štátom:&nbsp;<ShowNumberCurrency
+        num={data.total_final_value_amount_eur_sum}
+      />&nbsp;
+      <Badge color="primary">{data.count}</Badge>
+    </Button>
+    {toggledOn && (
+      <ul className="list-unstyled info-button-list">
+        {data.most_recent.map((notice) => (
+          <li key={notice.id} title={notice.title}>
+            {`${notice.client_name}, `}
+            <ShowNumberCurrency num={notice.total_final_value_amount} />
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+)
 
 export default compose(
   withState('toggledOn', 'toggle', false),
