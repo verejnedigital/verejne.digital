08e8ffb0df9b549ef7ae84afc1d826c16a639ed5
Richard <richardizip@gmail.com>
2018-08-22 19:01:59 +0200

    Show trade with state info
    * show entities which trade with state after selecting such address

--- client/src/components/Public/Map/AddressDetail/AddressDetail.js
+++ client/src/components/Public/Map/AddressDetail/AddressDetail.js
@@ -4,14 +4,17 @@ import {connect} from 'react-redux'
 import {withHandlers, compose} from 'recompose'
 import {withDataProviders} from 'data-provider'
 import {addressEntitiesProvider} from '../../../../dataProviders/publiclyDataProviders'
-import {addressEntitiesSelector} from '../../../../selectors'
+import {entityDetailProvider} from '../../../../dataProviders/sharedDataProviders'
+import {addressEntitiesSelector, addressEntitiesIdsSelector} from '../../../../selectors'
 import {closeAddressDetail} from '../../../../actions/publicActions'
 import {ListGroup, Button} from 'reactstrap'
-import {map} from 'lodash'
+import {map, chunk} from 'lodash'
 import ListRow from './ListRow'
 import './AddressDetail.css'
 
-const AddressDetail = ({entities, addressId, onClick}) => (
+const MAX_REQUEST_ID_COUNT = 50
+
+const AddressDetail = ({entities, addressId, onClick, hasStateTraders}) => (
   <div className="address-detail">
     <div className="address-detail-header">
       <Button color="link" onClick={onClick}>
@@ -26,14 +29,18 @@ const AddressDetail = ({entities, addressId, onClick}) => (
 
 export default compose(
   connect(
-    (state) => ({
+    (state, {addressId}) => ({
       entities: addressEntitiesSelector(state),
+      entitiesIds: addressEntitiesIdsSelector(state),
     }),
     {
       closeAddressDetail,
     }
   ),
   withDataProviders(({addressId}) => [addressEntitiesProvider(addressId)]),
+  withDataProviders(({entitiesIds}) => entitiesIds.length ?
+    chunk(entitiesIds, MAX_REQUEST_ID_COUNT).map((ids) => entityDetailProvider(ids)) : []
+  ),
   withHandlers({
     onClick: ({closeAddressDetail}) => (event) => {
       closeAddressDetail()
--- client/src/components/Public/entityHelpers.js
+++ client/src/components/Public/entityHelpers.js
@@ -1,5 +1,5 @@
 // @flow
-import type {CompanyEntity} from '../../state'
+import type {CompanyEntity, NewEntityDetail} from '../../state'
 
 export const isPolitician = (entity: CompanyEntity): boolean => {
   return entity.ds && !!entity.ds[0]
@@ -7,4 +7,11 @@ export const isPolitician = (entity: CompanyEntity): boolean => {
 
 export const hasContractsWithState = (entity: CompanyEntity): boolean => {
   return entity.ds && entity.ds[1] > 0
+}
+
+export const hasTradeWithState = (entityDetails: ?NewEntityDetail): boolean => {
+  return entityDetails && (
+    entityDetails.contracts != null ||
+    entityDetails.notices != null ||
+    entityDetails.eufunds != null)
 }
