0c5340bd08db3ffa2fa5bef09e4bf7c4e801b998
Jakub Ukrop <jakub.ukrop@vacuumlabs.com>
2018-08-23 10:06:28 +0200

    Update getInfos provider to get more infos at once

--- client/src/dataProviders/sharedDataProviders.js
+++ client/src/dataProviders/sharedDataProviders.js
@@ -1,10 +1,11 @@
 // @flow
 import React from 'react'
 import {receiveData} from '../actions/sharedActions'
-import {setEntityDetail} from '../actions/publicActions'
+import {setEntityDetails} from '../actions/publicActions'
 import {EntityDetailLoading} from '../components/Loading/Loading'
 import type {Company, NewEntityDetail} from '../state'
 import type {Dispatch} from '../types/reduxTypes'
+import type {ObjectMap} from '../types/commonTypes'
 
 const dispatchCompanyDetails = (eid: number) => (
   ref: string,
@@ -22,12 +23,12 @@ const dispatchEntitySearch = (query: string) => (
   dispatch(receiveData(['entitySearch'], {id: query, query, eids: data.map(({eid}) => eid)}, ref))
 }
 
-const dispatchEntityDetail = (eid: number) => (
+const dispatchEntityDetails = () => (
   ref: string,
-  data: NewEntityDetail,
+  data: ObjectMap<NewEntityDetail>,
   dispatch: Dispatch
 ) => {
-  dispatch(setEntityDetail(data[eid], eid))
+  dispatch(setEntityDetails(data))
 }
 
 export const companyDetailProvider = (eid: number, needed: boolean = true) => {
@@ -58,18 +59,18 @@ export const entitySearchProvider = (query: string) => ({
   onData: [dispatchEntitySearch, query],
 })
 
-export const entityDetailProvider = (eid: number, needed: boolean = true) => {
+export const entityDetailProvider = (eid: number | number[], needed: boolean = true) => {
   const requestPrefix = `${process.env.REACT_APP_API_URL || ''}`
   return {
-    ref: `entityDetail-${eid}`,
+    ref: `entityDetail-${eid.toString()}`,
     getData: [
       fetch,
-      `${requestPrefix}/api/v/getInfos?eids=${eid}`,
+      `${requestPrefix}/api/v/getInfos?eids=${eid.toString()}`,
       {
         accept: 'application/json',
       },
     ],
-    onData: [dispatchEntityDetail, eid],
+    onData: [dispatchEntityDetails],
     keepAliveFor: 60 * 60 * 1000,
     loadingComponent: <EntityDetailLoading />,
     needed,
