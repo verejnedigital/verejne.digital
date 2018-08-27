4216d5ad8d0fcd369c0ca49194523d16f582e248
PeterPsota <peter.n.psota@gmail.com>
2018-08-27 12:17:07 +0200

    Extracted EntitySearchAutocomplete from Sidebar

---
+++ client/src/components/Public/EntitySearchAutocomplete/EntitySearchAutocomplete.js
@@ -0,0 +1,88 @@
+// @flow
+import React from 'react'
+import {compose, withHandlers} from 'recompose'
+import {Input, Form, FormGroup, InputGroup, InputGroupAddon, Button} from 'reactstrap'
+import {connect} from 'react-redux'
+
+import SearchIcon from 'react-icons/lib/fa/search'
+import ModalIcon from 'react-icons/lib/fa/clone'
+
+import {
+  toggleModalOpen,
+  setEntitySearchFor,
+  toggleDrawer,
+  closeAddressDetail,
+} from '../../../actions/publicActions'
+import {updateValue} from '../../../actions/sharedActions'
+import {
+  entitySearchValueSelector,
+} from '../../../selectors'
+import {FIND_ENTITY_TITLE} from '../../../constants'
+
+type Props = {
+  toggleModalOpen: () => void,
+  entitySearchValue: string,
+  setEntitySearchValue: (e: Event) => void,
+  findEntities: (e: Event) => void,
+}
+
+const entitySearchAutocomplete = ({
+  toggleModalOpen,
+  entitySearchValue,
+  setEntitySearchValue,
+  findEntities,
+}: Props) => (
+  <Form onSubmit={findEntities}>
+    <FormGroup>
+      <InputGroup>
+        <Input
+          className="entity-input"
+          type="text"
+          placeholder={FIND_ENTITY_TITLE}
+          value={entitySearchValue}
+          onChange={setEntitySearchValue}
+        />
+        <InputGroupAddon addonType="append">
+          <Button color="primary" onClick={findEntities}>
+            <SearchIcon />
+          </Button>
+        </InputGroupAddon>
+        <InputGroupAddon addonType="append">
+          <Button color="primary" onClick={toggleModalOpen}>
+            <ModalIcon />
+          </Button>
+        </InputGroupAddon>
+      </InputGroup>
+    </FormGroup>
+  </Form>
+)
+
+export default compose(
+  connect(
+    (state) => ({
+      entitySearchValue: entitySearchValueSelector(state),
+    }),
+    {
+      updateValue,
+      toggleModalOpen,
+      setEntitySearchFor,
+      toggleDrawer,
+      closeAddressDetail,
+    }
+  ),
+  withHandlers({
+    findEntities: ({toggleModalOpen, setEntitySearchFor, entitySearchValue, toggleDrawer, closeAddressDetail}) => (e) => {
+      e.preventDefault()
+      toggleModalOpen()
+      closeAddressDetail()
+      setEntitySearchFor(entitySearchValue)
+      toggleDrawer()
+    },
+    setEntitySearchValue: ({updateValue}) => (e) =>
+      updateValue(
+        ['publicly', 'entitySearchValue'],
+        e.target.value,
+        'Set entity search field value'
+      ),
+  })
+)(entitySearchAutocomplete)
