d180100ddf64b2a2c3b67ff74e54cfe31494b465
Jozef Marko <jomarko@redhat.com>
2018-08-29 21:13:15 +0200

    Fix width of FB button

--- prepojenia/client/src/components/PrepojeniaPage.js
+++ prepojenia/client/src/components/PrepojeniaPage.js
@@ -137,7 +137,7 @@ class PrepojeniaPage extends Component {
                 <hr />
                 <div className="row">
                   <div className="col-sm-offset-2 col-sm-10 col-xs-offset-2 col-xs-10">
-                    <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fverejne.digital&width=111&layout=button_count&action=like&size=small&show_faces=true&share=true&height=46&appId=" width="151" height="23" className="fbIframe" scrolling="no" frameBorder="0" allowTransparency="true" />
+                    <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fverejne.digital&width=111&layout=button_count&action=like&size=small&show_faces=true&share=true&height=46&appId=" width="200" height="23" className="fbIframe" scrolling="no" frameBorder="0" allowTransparency="true" />
                   </div>
                 </div>
               </div>
