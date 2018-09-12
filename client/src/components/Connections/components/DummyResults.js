2dcf30d620269b9515930ac469e7f2750b3d46f9
PeterPsota <peter.n.psota@gmail.com>
2018-09-12 11:07:49 +0200

    Connections dummy results

---
+++ client/src/components/Connections/components/DummyResults.js
@@ -0,0 +1,19 @@
+// @flow
+import React from 'react'
+
+type Props = {
+  text: string,
+}
+
+const DummyResults = ({text}: Props) => (
+  <div className="row align-items-center">
+    <div className="col">
+      <h3 className="text-center">{text}</h3>
+    </div>
+  </div>
+)
+
+export const BeforeResults = () =>
+  DummyResults({text: 'Zadajte dvojicu pre začiatok vyhľadávania.'})
+export const EmptyResults = () =>
+  DummyResults({text: 'Prepojenie neexistuje.'})
