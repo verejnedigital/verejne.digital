84ea88f79d99c0e3892d276d704f9f3be1abffdc
mirecmrozek <miroslav.mrozek@vacuumlabs.com>
2018-09-20 12:34:24 +0200

    add date to relation title

--- client/src/components/shared/utilities.js
+++ client/src/components/shared/utilities.js
@@ -1,9 +1,21 @@
 export const showRelationType = (typeId: number, typeText: string, arrow: boolean) =>
   typeId == null ? 'Neznáme' : `${typeText || 'Neznáme'}${arrow ? typeId > 0 ? ' >' : ' <' : ''}`
 
-export const getRelationTitle = (typeId: number, name1: string, name2: string) =>
+export const getRelationTitle = (typeId: number, name1: string, name2: string, typeDate: string) =>
   typeId != null
-    ? typeId > 0
-      ? name1.concat(' zastupuje túto funkciu pre ').concat(name2)
-      : name2.concat(' zastupuje túto funkciu pre ').concat(name1)
+    ? typeDate === ''
+      ? typeId > 0
+        ? name1.concat(' zastupuje túto funkciu pre ').concat(name2)
+        : name2.concat(' zastupuje túto funkciu pre ').concat(name1)
+      : typeId > 0
+        ? name1
+          .concat(' zastupoval/a túto funkciu pre ')
+          .concat(name2)
+          .concat(' do ')
+          .concat(typeDate)
+        : name2
+          .concat(' zastupoval/a túto funkciu pre ')
+          .concat(name1)
+          .concat(' do ')
+          .concat(typeDate)
     : 'Spojenie neznáme'
