f91a258fefac177b193337045214a6102745e146
PeterPsota <peter.n.psota@gmail.com>
2018-09-27 16:36:42 +0200

    Recursive info closable

--- client/src/components/shared/CompanyDetails.js
+++ client/src/components/shared/CompanyDetails.js
@@ -4,6 +4,7 @@ import CompanyDetailWrapper from '../../dataWrappers/CompanyDetailWrapper'
 import type {CompanyDetailProps} from '../../dataWrappers/CompanyDetailWrapper'
 import Info from '../shared/Info/Info'
 
-const CompanyDetails = ({company}: CompanyDetailProps) => <Info data={company} />
+const CompanyDetails = ({company, canClose, onClose}: CompanyDetailProps) =>
+  <Info data={company} canClose={canClose} onClose={onClose} />
 
 export default CompanyDetailWrapper(CompanyDetails)
