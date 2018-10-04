5ac2cef3b77cba906a83c1800720dac204c6f99b
PeterPsota <peter.n.psota@gmail.com>
2018-10-04 11:53:58 +0200

    Special title for symmetric relation badges

--- client/src/components/shared/utilities.js
+++ client/src/components/shared/utilities.js
@@ -10,23 +10,33 @@ export const showRelationType = (
       arrow ? (typeId > 0 ? ' >' : ' <') : ''
     }`
 
-export const getRelationTitle = (typeId: number, name1: string, name2: string, typeDate: string) =>
+export const getRelationTitle = (
+  typeId: number,
+  name1: string,
+  name2: string,
+  typeDate: string,
+  symmetric: boolean
+) =>
   typeId != null
-    ? typeDate === ''
-      ? typeId > 0
-        ? name1.concat(' zastupuje túto funkciu pre ').concat(name2)
-        : name2.concat(' zastupuje túto funkciu pre ').concat(name1)
-      : typeId > 0
-        ? name1
-          .concat(' zastupoval/a túto funkciu pre ')
-          .concat(name2)
-          .concat(' do ')
-          .concat(typeDate)
-        : name2
-          .concat(' zastupoval/a túto funkciu pre ')
-          .concat(name1)
-          .concat(' do ')
-          .concat(typeDate)
+    ? symmetric
+      ? typeDate === ''
+        ? name1.concat(' a ').concat(name2).concat(' majú tento vzťah')
+        : name1.concat(' a ').concat(name2).concat(' mali tento vzťah do ').concat(typeDate)
+      : typeDate === ''
+        ? typeId > 0
+          ? name1.concat(' zastupuje túto funkciu pre ').concat(name2)
+          : name2.concat(' zastupuje túto funkciu pre ').concat(name1)
+        : typeId > 0
+          ? name1
+            .concat(' zastupoval/a túto funkciu pre ')
+            .concat(name2)
+            .concat(' do ')
+            .concat(typeDate)
+          : name2
+            .concat(' zastupoval/a túto funkciu pre ')
+            .concat(name1)
+            .concat(' do ')
+            .concat(typeDate)
     : 'Spojenie neznáme'
 
 export const getColor = (type: number, date: string) =>
