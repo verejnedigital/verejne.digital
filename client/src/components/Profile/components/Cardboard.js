c2b34b6b7d9a31057f5fc2c522fd1113a71ea822
PeterPsota <peter.n.psota@gmail.com>
2018-10-10 15:19:01 +0200

    Fix formatting

--- client/src/components/Profile/components/Cardboard.js
+++ client/src/components/Profile/components/Cardboard.js
@@ -13,10 +13,11 @@ type CardboardProps = {
 const mergeConsecutiveTerms = (offices: Array<PoliticianOffice>): Array<PoliticianOffice> => {
   let last = null
   return offices.reduce((acc, cur) => {
-    if (last
-      && last.term_start === cur.term_finish
-      && last.party_abbreviation === cur.party_abbreviation
-      && last.office_name_male === cur.office_name_male
+    if (
+      last &&
+      last.term_start === cur.term_finish &&
+      last.party_abbreviation === cur.party_abbreviation &&
+      last.office_name_male === cur.office_name_male
     ) {
       last.term_start = cur.term_start
     } else {
@@ -29,28 +30,35 @@ const mergeConsecutiveTerms = (offices: Array<PoliticianOffice>): Array<Politici
 
 const Cardboard = ({politician}: CardboardProps) => (
   <div className="profile-cardboard">
-    <img className="picture" src={politician.picture || '/politician_default.png'} alt="profilephoto" />
+    <img
+      className="picture"
+      src={politician.picture || '/politician_default.png'}
+      alt="profilephoto"
+    />
     <div>
       <h3 className="name">
-        {politician.firstname} {politician.surname}{politician.title && `, ${politician.title}`}
+        {politician.firstname} {politician.surname}
+        {politician.title && `, ${politician.title}`}
       </h3>
-      {politician.offices && mergeConsecutiveTerms(politician.offices).map((office, i) => (
-        <dl className="row" key={i}>
-          <dt className="col-md-1 col-sm-0">Funkcia</dt>
-          <dd className="col-md-11 col-sm-12">
-            {politician.surname.endsWith('ová')
-              ? office.office_name_female
-              : office.office_name_male
-            }{getTerm(office) && `, ${getTerm(office)}`}
-          </dd>
+      {politician.offices &&
+        mergeConsecutiveTerms(politician.offices).map((office, i) => (
+          <dl className="row" key={i}>
+            <dt className="col-md-1 col-sm-0">Funkcia</dt>
+            <dd className="col-md-11 col-sm-12">
+              {politician.surname.endsWith('ová')
+                ? office.office_name_female
+                : office.office_name_male}
+              {getTerm(office) && `, ${getTerm(office)}`}
+            </dd>
 
-          {office.party_nom &&
-            <Fragment>
-              <dt className="col-md-1 col-sm-0">Strana</dt>
-              <dd className="col-md-11 col-sm-12">{office.party_nom}</dd>
-            </Fragment>
-          }
-        </dl>))}
+            {office.party_nom && (
+              <Fragment>
+                <dt className="col-md-1 col-sm-0">Strana</dt>
+                <dd className="col-md-11 col-sm-12">{office.party_nom}</dd>
+              </Fragment>
+            )}
+          </dl>
+        ))}
     </div>
   </div>
 )
