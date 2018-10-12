e0c62b76647e219a4645a84edf10e95d5b7afb8d
PeterPsota <peter.n.psota@gmail.com>
2018-10-12 11:21:09 +0200

    Show year only once for terms ending within starting year

--- client/src/components/Profile/utilities.js
+++ client/src/components/Profile/utilities.js
@@ -3,7 +3,9 @@ import type {Politician, PoliticianDetail, PoliticianOffice} from '../../state'
 
 export const getTerm = (politician: Politician | PoliticianDetail | PoliticianOffice): string =>
   politician.term_start || politician.term_finish
-    ? `${politician.term_start || ''} - ${politician.term_finish || ''}`
+    ? politician.term_start === politician.term_finish
+      ? `${politician.term_start}`
+      : `${politician.term_start || ''} - ${politician.term_finish || ''}`
     : ''
 
 export const mergeConsecutiveTerms = (offices: Array<PoliticianOffice>): Array<PoliticianOffice> =>
