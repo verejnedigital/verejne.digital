461101c2f10fee221924244a9392eff0a606706c
Erik Kučák <35500848+Riko196@users.noreply.github.com>
2018-10-15 15:44:24 +0200

    Do not show "obdobie" and "strana" columns for political candidates. (#271)

--- client/src/components/Profile/components/Politician.js
+++ client/src/components/Profile/components/Politician.js
@@ -1,24 +1,31 @@
 // @flow
 import React from 'react'
 import {withHandlers} from 'recompose'
-import {NavLink} from 'react-router-dom'
-import {getTerm} from '../utilities'
+import {NavLink, type ContextRouter} from 'react-router-dom'
 
-import type {Politician as PoliticianType} from '../../../state'
+import {getTerm} from '../utilities'
+import {withRouter} from 'react-router'
+import {connect} from 'react-redux'
+import {compose} from 'redux'
+import {isItCandidatesListSelector} from '../../../selectors'
+import type {Politician as PoliticianType, State} from '../../../state'
 
 type PoliticianProps = {
   politician: PoliticianType,
   useDefaultPicture: Function,
+  isItCandidatesList: boolean,
 }
 
-const Politician = ({politician, useDefaultPicture}: PoliticianProps) => (
+const Politician = ({politician, useDefaultPicture, isItCandidatesList}: PoliticianProps) => (
   <tr className="table-line">
     <td className="number-column">{politician.order}.</td>
     <td className="photo-column">
       <img
         alt="foto"
         className="thumb-photo"
-        src={`https://verejne.digital/resources/profil_pics/${politician.surname}_${politician.firstname}.jpg`}
+        src={`https://verejne.digital/resources/profil_pics/${politician.surname}_${
+          politician.firstname
+        }.jpg`}
         onError={useDefaultPicture}
       />
     </td>
@@ -27,19 +34,27 @@ const Politician = ({politician, useDefaultPicture}: PoliticianProps) => (
         {politician.firstname} {politician.surname}
       </NavLink>
     </td>
-    <td className="text-left party-column">
-      {!politician.term_start && '\t'}
-      {getTerm(politician)}
-    </td>
-    <td className="party-column">{politician.party_abbreviation}</td>
+    {!isItCandidatesList && (
+      <td className="text-left party-column">
+        {!politician.term_start && '\t'}
+        {getTerm(politician)}
+      </td>
+    )}
+    {!isItCandidatesList && <td className="party-column">{politician.party_abbreviation}</td>}
     <td className="number-column">{politician.num_houses_flats}</td>
     <td className="number-column">{politician.num_fields_gardens}</td>
     <td className="number-column">{politician.num_others}</td>
   </tr>
 )
 
-export default withHandlers({
-  useDefaultPicture: () => (e) => {
-    e.target.src = '/politician_default_s.png'
-  },
-})(Politician)
+export default compose(
+  withRouter,
+  connect((state: State, props: ContextRouter) => ({
+    isItCandidatesList: isItCandidatesListSelector(state, props),
+  })),
+  withHandlers({
+    useDefaultPicture: ({history}) => (e) => {
+      e.target.src = '/politician_default_s.png'
+    },
+  })
+)(Politician)
