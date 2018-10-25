db33b3f79dea4baff4343a27c66938e46fdf0992
Rasto <rastislav.lenhardt@gmail.com>
2018-10-25 17:51:15 +0200

    delete old profil frontend

--- profil/client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Footer.css';
-
-const Footer = ({politician}) => (
-<div className="app-footer">
-O projekte | <a href="https://www.facebook.com/verejne.digital/">Kontaktujte nás cez facebook</a>
-</div>
-);
-export default Footer;
