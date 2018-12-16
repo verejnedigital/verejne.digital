6351b2d0e90af21679d8a529cb9dfb80777885bc
Matej Balog <matej.balog@gmail.com>
2018-12-16 21:08:45 +0100

    new public dumps: entities and related

--- data/prod_generation/generate_public_dumps.py
+++ data/prod_generation/generate_public_dumps.py
@@ -6,6 +6,7 @@ public_dumps.yaml.
 """
 
 import os
+import shutil
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../db')))
@@ -17,29 +18,33 @@ from utils import yaml_load
 def main():
   """Generates public data dump files from the latest prod data."""
 
-  # Connect to the latest production data schema:
+  # Connect to the latest schemas.
   db = DatabaseConnection(path_config='db_config.yaml')
   schema = db.get_latest_schema('prod_')
-  db.execute('SET search_path="' + schema + '";')
+  schema_profil = db.get_latest_schema('source_internal_profil_')
+  db.execute(
+      'SET search_path="' + schema + '", "' + schema_profil + '";')
   timestamp = schema[schema.rfind('_')+1:]
-  print('[OK] Dumping from schema "%s"...' % (schema))
+  print('[OK] Dumping from schemas "%s" and "%s"...' % (
+      schema, schema_profil))
 
-  # Read YAML configuration file:
+  # Read YAML configuration file.
   config = yaml_load('public_dumps.yaml')
   dir_save = config['save_directory']
   dumps = config['dumps']
 
-  # Process all dumps:
+  # Process all dumps.
   for dump_name in dumps:
-    save_paths = [
-        os.path.join(dir_save, dump_name + '.csv'),
-        os.path.join(dir_save, '%s_%s.csv' % (dump_name, timestamp))
-    ]
-    for save_path in save_paths:
-      db.dump_to_CSV(dumps[dump_name]['query'], save_path)
-      print('[OK] Saved dump "%s" to %s' % (dump_name, save_path))
+    save_path = os.path.join(
+        dir_save, '%s_%s.csv' % (dump_name, timestamp))
+    db.dump_to_CSV(dumps[dump_name]['query'], save_path)
+    print('[OK] Saved dump "%s" to %s' % (dump_name, save_path))
 
-  # Close database connection:
+    stage_path = os.path.join(dir_save, dump_name + '.csv')
+    shutil.copyfile(save_path, stage_path)
+    print('[OK] Copied dump "%s" to %s' % (dump_name, stage_path))
+
+  # Close database connection.
   db.close()
 
 
