81f593e6bb1e66ed0259b1013bcac79d86d71c03
Tomas Fabry <tomas.fabry@vacuumlabs.com>
2019-01-24 11:13:20 +0100

    Notices legend moved above the search + redesign

--- client/src/components/Notices/Legend.js
+++ client/src/components/Notices/Legend.js
@@ -8,28 +8,13 @@ import './LegendSymbols.css'
 
 const Legend = () => (
   <div className="notice-legend">
-    <Row>
-      <Col xs={{size: 10, offset: 2}}>
-        <h3 className="notice-legend-label">Legenda:</h3>
-      </Col>
-    </Row>
-    <Row tag="dl">
-      <Col tag="dt" xs="2" className="text-right px-0">
-        {formatSimilarPercent(19)}
-      </Col>
-
-      <Col tag="dd" xs="10">
-        podobnosť s predchádzajúcim obstarávaním
-      </Col>
-
-      <Col tag="dt" xs="2" className="text-right px-0">
-        {getWarningSymbol(1)}
-      </Col>
-
-      <Col tag="dd" xs="10">
-        podozrivé obstarávanie
-      </Col>
-    </Row>
+    <h3 className="notice-legend-label">Legenda:</h3>
+    <dl className="notice-legend-items">
+      <dt>{formatSimilarPercent(19)}</dt>
+      <dd>podobnosť s predchádzajúcim obstarávaním</dd>
+      <dt>{getWarningSymbol(1)}</dt>
+      <dd>podozrivé obstarávanie</dd>
+    </dl>
   </div>
 )
 
